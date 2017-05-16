6b41bb78e63440f99c421c77c786563d774222e2
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-16 12:08:18 +0100

    set-up server.js for our back-end except for using our routes

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-// Dependencies 
+// Dependencies
 const express    = require('express');
 const morgan     = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose   = require('mongoose');
 const cors       = require('cors');
 const app        = express();
+
+const config     = require('./config/config');
+
+// Connects to our database
+mongoose.connect(config.db, () => {
+  console.log(`Connected to ${config.db}`);
+});
+
+// Setting up our middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+// Listens on port
+app.listen(config.port, () => {
+  console.log(`We are listening on port: ${config.port}`);
+})
