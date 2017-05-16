7cee74251685ac1c89358827fdebf9a236a68004
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-16 16:39:08 +0100

    a clientsCreate function to clients.js

--- controllers/clients.js
+++ controllers/clients.js
@@ -7,7 +7,16 @@ function clientIndex(req, res){
   });
 }
 
+function clientsCreate(req, res){
+  const client = new Client(req.body.client);
+
+  client.save((err, client) => {
+    if(err) return res.status(500).json(err);
+    return res.status(201).json(client);
+  });
+};
 
 module.exports = {
   index: clientIndex,
+  create: clientsCreate
 };
