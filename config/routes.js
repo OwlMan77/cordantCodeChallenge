2dca9e07a65f9d8c167e2b5da96b0b65578bde92
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-16 16:23:13 +0100

    added routes for candidate show, delete and update

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const router  = express.Router();
+const express    = require('express');
+const router     = express.Router();
 
 const candidates = require('../controllers/candidates');
 
 router.route('/')
+
 router.route('/candidates')
   .get(candidates.index)
   .post(candidates.create);
-
+router.route('/candidates/:id')
+  .get(candidates.show)
+  .put(candidates.update)
+  .delete(candidates.delete);
 
 module.export = router;
