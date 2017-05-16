ac2c6912ace97414c1a52e8da5e9dca707119af6
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-16 11:51:02 +0100

    added intial dependencies to server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+// Dependencies 
+const express    = require('express');
+const morgan     = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose   = require('mongoose');
+const cors       = require('cors');
+const app        = express();
