a5c22eb9ba53bba9902f5d85c30bc9c207d877df
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-16 15:56:10 +0100

    candidates update added to candidate.js

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -7,6 +7,32 @@ function candidatesCreate(req, res){
   });
 };
 
+function candidatesIndex(req, res){
+  Candidate.find({}, (err, users) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(users);
+  });
+}
+function candidatesShow(req, res){
+  Candidate.findById(req.params.id, (err, show) => {
+    if (err) return res.status(500).json({ success: false, message: err})
+    if(!Candidate) return res.status(500).json({success: false, message: 'Person does not exist here'})
+    return res.stat(200).json(candidate);
+  })
+}
+
+function candidatesUpdate(req, res){
+  Candidate.findByIdAndUpdate(req.params.id, req.body.candidates, (err, candidate) => {
+    if (err) return res.status(500).json({ success: false, message: err });
+    if (!candidate) return res.status(500).json({ success: false, message: 'No candidate found' });
+    return res.status(204).json({ success: true });
+  })
+}
+
+
 module.exports = {
-  create: candidatesCreate
+  create: candidatesCreate,
+  index: candidatesIndex,
+  show: candidatesShow,
+  update: candidatesUpdate
 }
