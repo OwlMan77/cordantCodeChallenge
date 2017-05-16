cd5fc4ed706fe2ea499cf9c96340e81d7516a36a
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-16 12:29:14 +0100

    added a schema to client.js

--- models/client.js
+++ models/client.js
@@ -1,1 +1,8 @@
 const mongoose = require('mongoose');
+
+const clientSchema = new mongoose.Schema({
+  name: { type: String, trim: true, required: true},
+  postcode: {type: String, maxlength: 9}
+});
+
+module.exports = mongoose.model('Client', clientSchema);
