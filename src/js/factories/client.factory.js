25b988c03df0ab174f8d37a824b74382e00ec99a
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-17 12:11:29 +0100

    defined client.factory.js

---
+++ src/js/factories/client.factory.js
@@ -0,0 +1,17 @@
+angular
+.module('cordant')
+.factory('Client', clientFactory);
+
+clientFactory.$inject = ['API', $resource];
+
+function clientFactory(API, $resource){
+
+  return $resource(`${API}/clients/:id`, {id: '@_id'},     {
+      'get': { method: 'GET', isArray: false },
+      'save': { method: 'POST' },
+      'query': { method: 'GET', isArray: true },
+      'remove': { method: 'DELETE' },
+      'delete': { method: 'DELETE' }
+    }
+  );
+}
