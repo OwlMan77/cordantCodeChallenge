25d87d78051769e967a0025e6cb99841f1af1560
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-17 12:10:32 +0100

    defined candidate.factory.js

---
+++ src/js/factories/candidate.factory.js
@@ -0,0 +1,17 @@
+angular
+.module('cordant')
+.factory('Candidate', candidateFactory);
+
+candidateFactory.$inject = ['API', $resource];
+
+function candidateFactory(API, $resource){
+
+  return $resource(`${API}/candidates/:id`, {id: '@_id'},     {
+      'get': { method: 'GET', isArray: false },
+      'save': { method: 'POST' },
+      'query': { method: 'GET', isArray: true },
+      'remove': { method: 'DELETE' },
+      'delete': { method: 'DELETE' }
+    }
+  );
+}
