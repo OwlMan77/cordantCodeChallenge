5b52becf24cf52d8d173a8ef582dcbdf2420a374
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-17 16:21:06 +0100

    defined show controller

---
+++ src/js/controllers/clients/show.controller.js
@@ -0,0 +1,13 @@
+angular
+.module('cordant')
+.controller('clientShowCtrl', showCtrl);
+
+showCtrl.$inject = ['Client', '$stateParams'];
+function showCtrl(Client, $stateParams){
+ const vm = this;
+  Client
+  .get($stateParams)
+  .$promise.then(response => {
+    vm.user = response;
+  });
+}
