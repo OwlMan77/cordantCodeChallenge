d4047f643b5d9cb4410a2ffb7134b6af1f884cdb
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-17 16:24:38 +0100

    added state for clientShow

--- src/js/config/router.config.js
+++ src/js/config/router.config.js
@@ -4,18 +4,24 @@ angular
 
 Router.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
 function Router($stateProvider, $locationProvider, $urlRouterProvider){
-    $locationProvider.html5Mode(true);
-    $stateProvider
+  $locationProvider.html5Mode(true);
+  $stateProvider
     .state('home', {
       url: '/',
       templateUrl: '/js/views/home.html'
     })
-    .state('ClientSelector',
-  {
-    url: '/clients',
-    templateUrl: '/js/views/clients/index.html',
-    controller: 'clientIndexCtrl',
-    controllerAs: 'clientIndex'
-  });
+    .state('ClientSelector', {
+      url: '/clients',
+      templateUrl: '/js/views/clients/index.html',
+      controller: 'clientIndexCtrl',
+      controllerAs: 'clientIndex'
+      })
+    .state('ClientPage', {
+      url: '/clients/:id',
+      templateUrl: '/js/views/clients/show.html',
+      controller: 'clientShowCtrl',
+      controllerAs: 'clientShow'
+    });
+
   $urlRouterProvider.otherwise('/');
 }
