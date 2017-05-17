60cb9c1a166e61ee4e671ab8417476c99d457bc4
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-17 13:20:51 +0100

    added and defined candidate index.controller

---
+++ src/js/controllers/candidates/index.controller.js
@@ -0,0 +1,9 @@
+angular
+  .module('cordant')
+  .controller('clientIndexCtrl', indexCtrl)
+
+  indexCtrl.$inject = ['Candidate'];
+  function indexCtrl(Candidate){
+    const vm = this;
+    vm.candidates = Candidate.query();
+  }
