bf9da2ba14329558ef08b4a55e927eca466c839a
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-20 13:46:53 +0100

    added to candidate index controller to assign candidate geolocation on creation

--- src/js/controllers/candidates/index.controller.js
+++ src/js/controllers/candidates/index.controller.js
@@ -2,8 +2,20 @@ angular
   .module('cordant')
   .controller('clientIndexCtrl', indexCtrl);
 
-  indexCtrl.$inject = ['Candidate'];
-  function indexCtrl(Candidate){
-    const vm = this;
+  indexCtrl.$inject = ['Candidate', 'Postcode'];
+  function indexCtrl(Candidate, postcode){
+    const vm      = this;
     vm.candidates = Candidate.query();
+
+    vm.getLatLng = () => {
+      Postcode
+      .get({{id: vm.candidate.postcode}})
+      .$promise
+      .then(response => {
+        vm.candidate.latitude = response.result.latitude,
+        vm.candidate.longitude = response.result.longitude
+      })
+    }
+
+    vm.create = () => Candidate.save(vm.candidate).then( console.log(`${vm.candidate.latitude}`));
   }
