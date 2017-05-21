a9996ab107529b2d0a37f1607463a862b93aea2b
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-21 20:52:05 +0100

    made a create Candidate function

--- src/js/controllers/clients/index.controller.js
+++ src/js/controllers/clients/index.controller.js
@@ -2,13 +2,34 @@ angular
   .module('cordant')
   .controller('clientIndexCtrl', indexCtrl);
 
-  indexCtrl.$inject = ['Client','Candidate'];
-  function indexCtrl(Client, Candidate){
+  indexCtrl.$inject = ['Client','Candidate', 'Postcode'];
+  function indexCtrl(Client, Candidate, Postcode){
     const vm   = this;
 
     //index api call
     vm.clients = Client.query();
     vm.candidates = Candidate.query();
+
+    vm.candidateLatLng = () => {
+      Postcode
+      .get({id:vm.candidate.postcode})
+      .$promise
+      .then(response =>
+        {
+          vm.candidate.latitude = response.result.latitude,
+          vm.candidate.longitude = response.result.longitude
+        }
+      );
+    }
+
+    vm.createCandidate = () =>{
+      Candidate
+      .save(vm.candidate)
+      .$promise
+      .then(() => {
+        location.reload();
+      });
+    }
 
     //post request to create a new client
     vm.create = () => {
